c7390761fbf4c089266202d3f023694709f9ea7b
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-20 20:42:29 -0300

    Adiciona leitura dos dados do banco

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import json
 import mysql.connector
 
-# pega configurações do arquivo de configuração
+class Agenda:
+
+    def __init__(self, codigo, nome):
+        self.codigo = codigo
+        self.nome = nome
+        self.telefones = []
+        self.emails = []
+
+# carrega configurações
 with open('config.json') as file:
     config = json.load(file)
 
@@ -12,6 +20,34 @@ cnx = mysql.connector.connect(
     host = config['host'],
     database = config['schema']
 )
+cursor = cnx.cursor()
 
-# fecha a conecção
+cursor.execute(('select codigo, nome from agenda;'))
+qagendas = [agenda for agenda in cursor]
+
+cursor.execute(('select codigo, email from email;'))
+qemails = [email for email in cursor]
+
+cursor.execute(('select codigo, telefone from telefone;'))
+qtelefones = [telefone for telefone in cursor]
+
+agendas = {}
+
+for (codigo, nome) in qagendas:
+    agendas[codigo] = Agenda(codigo, nome)
+
+for (codigo, email) in qemails:
+    agendas[codigo].emails.append(email)
+
+for (codigo, telefone) in qtelefones:
+    agendas[codigo].telefones.append(telefone)
+
+for codigo, agenda in agendas.items():
+    print(agenda.nome)
+    print(agenda.emails)
+    print(agenda.telefones)
+    print()
+
+# fecha a conexão
+cursor.close()
 cnx.close()
