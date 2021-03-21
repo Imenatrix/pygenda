190e332617f25a943b3d15de06a31a1dfd95594b
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-21 16:33:52 -0300

    Adiciona interatividade em agenda.render

--- agenda.py
+++ agenda.py
@@ -1,8 +1,42 @@
+import os
+from getch import getch
+
 def render(agenda):
-    print(f'Nome: {agenda.nome}')
-    print('--------------------')
-    print('[ Emails ] Telefones')
-    print('--------------------')
-    
-    for email in agenda.emails:
-        print(email)
+
+    selection = 0
+
+    while True:
+        os.system('clear')
+        print(f'Nome: {agenda.nome}')
+        print('--------------------')
+        if selection == 0:
+            print('[ Emails ]  Telefones')
+        elif selection == 1:
+            print('  Emails  [ Telefones ]')
+        print('--------------------')
+
+        if selection == 0:
+            for email in agenda.emails:
+                print(email)
+        elif selection == 1:
+            for telefone in agenda.telefones:
+                print(telefone)
+        
+        # entrada do teclado
+        key = handleKeyboardInput()
+
+        # busca por pelas setas
+        if key == b'\x1b[C':
+            selection += 1
+            if selection == 2:
+                selection = 0
+        elif key == b'\x1b[D':
+            selection -= 1
+            if selection == -1:
+                selection = 1
+
+def handleKeyboardInput():
+    key = getch()
+    if bytes(key, 'utf-8') == b'\x1b':
+        key += getch() + getch()
+    return bytes(key, 'utf-8')
