c2a2d6e95e6feb3a790b76c533ea2e90cc82a077
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-03-21 22:58:59 -0300

    Refatoração em context

--- context.py
+++ context.py
@@ -24,13 +24,13 @@ class Context:
 
         cursor = self.cursor
 
-        cursor.execute(('select codigo, nome from agenda;'))
+        cursor.execute(('select codigo, nome from agenda'))
         qagendas = [agenda for agenda in cursor]
 
-        cursor.execute(('select codigo, email from email;'))
+        cursor.execute(('select codigo, email from email'))
         qemails = [email for email in cursor]
 
-        cursor.execute(('select codigo, telefone from telefone;'))
+        cursor.execute(('select codigo, telefone from telefone'))
         qtelefones = [telefone for telefone in cursor]
 
         agendas = {}
@@ -47,79 +47,79 @@ class Context:
         self.agendas = agendas
 
     def createAgenda(self, agenda):
-        
-        cursor = self.cursor
-
-        cursor.execute(('insert into agenda(nome) values (%s);'), (agenda.nome, ))
-        agenda.codigo = cursor.lastrowid
+        self.cursor.execute(
+            ('insert into agenda(nome) values (%s)'),
+            (agenda.nome, )
+        )
+        agenda.codigo = self.cursor.lastrowid
         self.agendas[agenda.codigo] = agenda
-
         self.connection.commit()
 
     def updateAgenda(self, agenda):
-
-        cursor = self.cursor
-
-        cursor.execute(('update agenda set nome = %s where codigo = %s'), (agenda.nome, agenda.codigo))
+        self.cursor.execute(
+            ('update agenda set nome = %s where codigo = %s'),
+            (agenda.nome, agenda.codigo)
+        )
+        self.agendas[agenda.codigo].nome = agenda.nome
         self.connection.commit()
 
     def deleteAgenda(self, agenda):
-
-        cursor = self.cursor
-
-        cursor.execure(('delete from agenda where codigo = %s'), (agenda.codigo, ))
+        self.cursor.execute(
+            ('delete from agenda where codigo = %s'),
+            (agenda.codigo, )
+        )
+        self.agendas.remove(agenda)
         self.connection.commit()
 
-
     def createEmail(self, codigo, email):
-
-        cursor = self.cursor
-
-        cursor.execute(('insert into email(codigo, email) values (%s, %s)'), (codigo, email))
+        self.cursor.execute(
+            ('insert into email(codigo, email) values (%s, %s)'),
+            (codigo, email)
+        )
         self.agendas[codigo].emails.append(email)
         self.connection.commit()
 
     def updateEmail(self, codigo, email, newEmail):
-
-        cursor = self.cursor
-
-        cursor.execute(('update email set email = %s where codigo = %s and email = %s'), (newEmail, codigo, email))
+        self.cursor.execute(
+            ('update email set email = %s where codigo = %s and email = %s'),
+            (newEmail, codigo, email)
+        )
         index = self.agendas[codigo].emails.index(email)
         self.agendas[codigo].emails[index] = newEmail
         self.connection.commit()
 
     def deleteEmail(self, codigo, email):
-
-        cursor = self.cursor
-
-        cursor.execute(('delete from email where codigo = %s and email = %s'), (codigo, email))
+        self.cursor.execute(
+            ('delete from email where codigo = %s and email = %s'),
+            (codigo, email)
+        )
         self.agendas[codigo].emails.remove(email)
         self.connection.commit()
 
     def createTelefone(self, codigo, telefone):
-        
-        cursor = self.cursor
-
-        cursor.execute(('insert into telefone(codigo, telefone) values (%s, %s)'), (codigo, telefone))
+        self.cursor.execute(
+            ('insert into telefone(codigo, telefone) values (%s, %s)'),
+            (codigo, telefone)
+        )
         self.agendas[codigo].telefones.append(telefone)
         self.connection.commit()
 
     def updateTelefone(self, codigo, telefone, newTelefone):
-
-        cursor = self.cursor
-
-        cursor.execute(('update telefone set telefone = %s where codigo = %s and telefone = %s'), (newTelefone, codigo, telefone))
+        self.cursor.execute(
+            ('update telefone set telefone = %s where codigo = %s and telefone = %s'),
+            (newTelefone, codigo, telefone)
+        )
         index = self.agendas[codigo].telefones.index(telefone)
         self.agendas[codigo].telefones[index] = newTelefone
         self.connection.commit()
 
     def deleteTelefone(self, codigo, telefone):
-
-        cursor = self.cursor
-        cursor.execure(('delete from telefone where codigo = %s and telefone = %s'), (codigo, telefone))
+        self.cursor.execure(
+            ('delete from telefone where codigo = %s and telefone = %s'),
+            (codigo, telefone)
+        )
         self.agendas[codigo].telefones.remove(telefone)
         self.connection.commit()
-
     
     def drop(self):
         self.cursor.close()
